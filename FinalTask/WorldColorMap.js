a7c893cda27f608ecdfef9467d49d8c324c19958
kawanishi <kawanishi@fine.cs.kobe-u.ac.jp>
2021-06-11 23:45:05 +0900

    Before combine svgs

--- FinalTask/WorldColorMap.js
+++ FinalTask/WorldColorMap.js
@@ -1,19 +1,91 @@
 
 class WorldColorMap {
 
-    constructor() {
+    // convert country name defined by world map to defined by data file.
+    name_filter = function(name) {
+        if(name == "United States of America")
+            return "United States"
+        return name;
+    };
 
+    constructor(config, data, world_map) {
+        this.config = {
+            parent: config.parent,
+            width : config.width  || 256,
+            height: config.height || 256,
+            scale : config.scale  || 50,
+            margin: config.margin || {top:10, right:10, bottom:10, left:10},
+            value : config.value  || function(d){ return d.cases; },
+            label : config.label  || ``,
+        }
+        this.data       = data;
+        this.world_map  = world_map;
+        this.init();
     }
 
     init() {
+        let self = this;
+
+        self.svg = d3.select( self.config.parent )
+            .attr('width', self.config.width)
+            .attr('height', self.config.height);
+
+        self.chart = self.svg.append('g')
+            .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`);
 
     }
 
     update() {
+        let self = this;
 
+        self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
+        self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
+
+        // world map
+        self.projection = d3.geoMercator()
+            .scale(self.config.scale)
+            .center([0, 0])  
+            .translate([self.inner_width / 2, self.inner_height / 2]);
+
+        self.path = d3.geoPath().projection(self.projection);
+
+        // color scale
+        self.min = d3.min(data, self.config.value);
+        self.max = d3.max(data, self.config.value);
+
+        self.cscale = d3.scaleSequential(d3.interpolateYlOrRd)
+            .domain([self.min, self.max]);
+
+        self.colorByName = ( name => {
+            var d = self.data.find( d => { return d.country == self.name_filter(name); });
+            if( d == null ) return "white";
+            return self.cscale(self.config.value(d));
+        });
+
+        self.render();
     }
 
-    renderer() {
-        
+    render() {
+        let self = this;
+
+        // world map
+        self.chart.selectAll('path')
+            .data(world_map.features)
+            .enter()
+            .append('path')
+            .attr('d', self.path)
+            .style("fill", (country) => {
+                return self.colorByName(country.properties.NAME);
+            })
+            .attr("stroke", "black")
+            
+
+        // label
+        self.chart.append('text')
+            .style('font-size', '16px')
+            .attr('x', self.config.margin.left + self.inner_width / 2)
+            .attr('y', self.inner_height + self.config.margin.top + 20)
+            .attr('text-anchor', 'middle')
+            .text( self.config.label );
     }
 }
--- FinalTask/main.js
+++ FinalTask/main.js
@@ -1,16 +1,87 @@
-d3.csv("https://218x107x.github.io/InfoVis2021/FinalTask/data/COVID-19-geographic-disbtribution-worldwide.csv")
-    .then( data => {
-        // Convert strings to numbers
-        // data.forEach( d => { d.x = +d.x; d.y = +d.y; });
-        var config = {
-            width: 512,
-            height: 256,
-            margin: {top:10, right:10, bottom:20, left:40},
-            animation_duration: 1000
-        }
-        
+d3.csv("https://covid.ourworldindata.org/data/latest/owid-covid-latest.csv")
+    .then( data1 => {
+        d3.csv("https://218x107x.github.io/InfoVis2021/FinalTask/data/WHR20_DataForFigure2.1.csv")
+            .then( data2 => {
+                d3.json("https://218x107x.github.io/InfoVis2021/FinalTask/data/countries.json")
+                    .then( map => {
+                        COVID_input_data = data1;
+                        HAPPINESS_input_data = data2;
+                        world_map = map;
+                        // convert string to number
+                        COVID_input_data.forEach( d => {
+                            d.total_cases = +d.total_cases;
+                        });
+                        HAPPINESS_input_data.forEach( d => {
+                            d[`Ladder score`] = +d[`Ladder score`];
+                        });
+
+                        Filter();
+                        main();
+                    })
+            })
     })
     .catch( error => {
         console.log( error );
     }
 );
+
+function Filter() { 
+    // Extract countries that exist in both data.
+    filter = [];
+    HAPPINESS_input_data.forEach(d => {
+        if(!filter.includes(d[`Country name`])){
+            filter.push(d[`Country name`]);
+        }
+    });
+    COVID_data = COVID_input_data.filter( d => {
+        return filter.includes(d.location);
+    });
+}
+
+function main() {
+     // Combine the two data
+     data = [];
+     COVID_data.forEach( cd => {
+         hd = HAPPINESS_input_data.find( d => cd.location === d[`Country name`]);
+         if(hd == null) return;         
+         data.push({
+             country    : cd.location,
+             cases      : cd.total_cases / cd.population,
+             score      : hd[`Ladder score`],
+             population : cd.population,
+         });
+     });
+
+    scatter_plot = new ScatterPlot({
+        parent: '#drawing_region_scatterplot',
+        width : 512,
+        height: 512,
+        margin: {top:10, right:10, bottom:100, left:100},
+        xlabel: 'happiness score',
+        ylabel: 'COVID cases / population',
+        fontsize: `16px`
+    }, data);
+    scatter_plot.update();
+
+    happiness_world_map = new WorldColorMap({
+        parent: '#drawing_region_happiness_worldmap',
+        width : 512,
+        height: 512,
+        scale : 64,
+        margin: {top:10, right:50, bottom:100, left:10},
+        value : function(d){ return d.cases; },
+        label : `COVID cases for each country`
+    }, data, world_map);
+    happiness_world_map.update();
+
+    covid_world_map = new WorldColorMap({
+        parent: '#drawing_region_covid_worldmap',
+        width : 512,
+        height: 512,
+        scale : 64,
+        margin: {top:10, right:50, bottom:100, left:10},
+        value : function(d){ return d.score; },
+        label : `Happiness score for each country`
+    }, data, world_map);
+    covid_world_map.update();
+}
